53e9de87848f6a9eeda0ac7eba2ba128ca75b388
Haomiao2000 <43301857+HaomiaoLiu730@users.noreply.github.com>
2021-02-24 11:19:47 -0500

    Revert "Revert "add opening screen and loading screen""

---
+++ core/src/edu/cornell/gdiac/main/controller/gaming/GameMode.java
@@ -0,0 +1,41 @@
+package edu.cornell.gdiac.main.controller.gaming;
+
+import edu.cornell.gdiac.assets.AssetDirectory;
+import edu.cornell.gdiac.main.controller.ModeController;
+import edu.cornell.gdiac.main.view.GameCanvas;
+
+public class GameMode implements ModeController {
+    /**
+     * Creates a new game with a playing field of the given size.
+     * <p>
+     * This constructor initializes the models and controllers for the game.  The
+     * view has already been initialized by the root class.
+     *
+     * @param width  The width of the game window
+     * @param height The height of the game window
+     * @param assets The asset directory containing all the loaded assets
+     */
+    public GameMode(float width, float height, AssetDirectory assets) {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void draw(GameCanvas canvas) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+}
---
+++ core/src/edu/cornell/gdiac/main/controller/opening/Loading.java
@@ -0,0 +1,18 @@
+package edu.cornell.gdiac.main.controller.opening;
+
+import edu.cornell.gdiac.assets.AssetDirectory;
+import edu.cornell.gdiac.main.controller.ModeController;
+
+public interface Loading extends ModeController {
+    /**
+     * get the state of this controller mode, return true if ready
+     * @return boolean
+     */
+    public boolean isReady();
+
+    /**
+     * get the asset directory for next mode
+     * @return AssetDirectory
+     */
+    public AssetDirectory getAssets();
+}
